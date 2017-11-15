f257145769f6ab0b836ae2c7c97ae977e5e6f5c5
thinh <thinh.t.nguyen@sjsu.edu>
2017-11-14 19:17:23 -0800

    Rebuild the model as the instruction sent by Kunal

--- conv_train.py
+++ conv_train.py
@@ -1,83 +1,76 @@
 import tensorflow as tf
+import numpy as np
 import sys
 import util
 
 
 cost_mode = ['cross', 'cross-l1', 'cross-l2']
 
-num_classes = 5
-num_inputs = 25 * 25
+num_classes = 5  # 5 letters at the output
+image_size = 25  # image size is 25 x 25
 stride = 1
 
-
 # tf Graph input
-input_holder = tf.placeholder(tf.float32, [None, num_inputs])
-output_holder = tf.placeholder(tf.float32, [None, num_classes])
+input_holder = tf.placeholder(tf.float32, [None, image_size, image_size, 1],
+                              name='x')
+output_holder = tf.placeholder(tf.float32, [None, num_classes], name='y')
 
 
-def initialize_weights_biases(layers):
-    # layer_def is a dictionary store the network description. For example,
-    # {1: [5, 4], 2:[6, 8]}. Mean that layer 1, feature size is 5, and 4 is
-    # the number of features“
-    if not layers:
-        weights = dict()
-        biases = dict()
-        max_key = 0
-        input_num = 1
-        output_num = 0
-        for k in layers.keys:
-            # Get the feature size and number of features from layer
-            # definition.
-            fn = layers[k]
-            filter_num = fn[0]
-            output_num = (input_num - filter_num)/stride + 1
-            if max_key < k:
-                # Convolutional layer
-                # weight key, and bias key for each layer
-                weights[k] = tf.Variable(tf.random_normal(
-                    [filter_num, filter_num, input_num, output_num]))
-                biases[k] = tf.Variable(tf.random_normal([fn[1]]))
-            else:
-                # Dense layer
-                weights['dense'] = tf.Variable(tf.random_normal([input_num, fn[0]]))
-                biases['dense'] = tf.Variable(tf.random_normal([fn[0]])),
-            max_key = k
-            input_num = output_num
-        weights['out'] = tf.Variable(tf.random_normal([output_num, num_classes]))
-        biases['out'] = tf.Variable(tf.random_normal([num_classes]))
-        return weights, biases
+def create_weight(shape):
+    return tf.Variable(tf.truncated_normal(shape))
 
 
-def build_conv_layer(x, w, b, feature_size, num_features):
-    x = tf.nn.conv2d(x, w, strides=[1, stride, stride, 1], padding='SAME')
-    x = tf.nn.bias_add(x, b)
-    return tf.nn.relu(x)
+def create_bias(size):
+    # TODO: generate bias in the range as described on slide Nov 13
+    return tf.Variable(tf.constant(0.05, shape=[size]))
 
 
-def maxpool2d(x, k=2):
-    # MaxPool2D wrapper
-    return tf.nn.max_pool(x, ksize=[1, k, k, 1], strides=[1, k, k, 1],
-                          padding='SAME')
+def create_conv_layer(input_layer, filter_size, num_filters, num_channels):
+    weights = create_weight(shape=[filter_size, filter_size,
+                                   num_channels, num_filters])
+    biases = create_bias(num_filters)
+    layer = tf.nn.conv2d(input=input_layer, filter=weights,
+                         strides=[1, stride, stride, 1], padding='SAME')
+    layer = tf.nn.bias_add(layer, biases)
+    # Need the setting for k ?, most the documents say 2 is good
+    layer = tf.nn.max_pool(value=layer, ksize=[1, 2, 2, 1],
+                           stride=[1, stride, stride, 1], padding="SAME")
+    return tf.nn.relu(layer)
 
 
-def build_model(x, cost, layers):
-    x = tf.reshape(x, shape=[-1, 25, 25, 1])
+def create_flatten_layer(layer):
+    shape = layer.get_shape()
+    # TODO: have to understand the magic here
+    num_features = shape[1:4].num_elements()
+    layer = tf.reshape(layer, [-1, num_features])
+    return layer
+
+
+def create_fully_connected_layer(input_layer, input_size, output_size):
+    weights = create_weight(shape=[input_size, output_size])
+    biases = create_bias(output_size)
+    layer = tf.matmul(input_layer, weights) + biases
+    return tf.nn.relu(layer)
+
+
+def build_model(layers_def):
     # Convolution Layer
-    weights, biases = initialize_weights_biases(layers)
-    convolutional_layer = None
-    for w in weights.keys():
-        if w != 'dense' or w != 'out':
-            convolutional_layer = build_conv_layer(x, weights[w], biases[w])
-            # Max Pooling (down-sampling)
-            convolutional_layer = maxpool2d(convolutional_layer, k=2)
-
-    full_connected = tf.reshape(convolutional_layer, [-1, weights['dense'].get_shape().as_list()[0]])
-    full_connected = tf.add(tf.matmul(full_connected, weights['dense']), biases['dense'])
-    full_connected = tf.nn.relu(full_connected)
-
-    # Output, class prediction
-    out = tf.add(tf.matmul(full_connected, weights['out']), biases['out'])
-    return out
+    layer = None
+    for i in layers_def.keys():
+        fn = layers_def[i]
+        if len(fn) >= 1:  # This is not the last layer yet
+            filter_size = fn[0]
+            filter_num = fn[1]
+            if i == 1:  # 1st Layer receive input holder
+                layer = create_conv_layer(
+                    input_holder, filter_size, filter_num, 1)
+            else:  # 2nd and above layers receive previous layer as input
+                layer = create_conv_layer(layer, filter_size, filter_num, 1)
+        else:  # Last layer in network description file
+            layer = create_flatten_layer(layer)
+            layer = create_fully_connected_layer(layer, filter_size,
+                                                 num_classes)
+    return layer
 
 
 def validate_arguments(arguments):
@@ -97,9 +90,14 @@ if __name__ == "__main__":
     if validate_arguments(sys.argv):
         cost = sys.argv[1]
         network_description = sys.argv[2]
-
     else:
         sys.exit("Invalid Arguments")
 
     layer_def = util.load_layers_definition(network_description)
-    layer1 = layer_def[1]
+    x = np.random.randint(3, size=(25, 25))
+    model = build_model(layer_def)
+
+    session = tf.Session()
+    init = tf.global_variables_initializer()
+    session.run(init)
+    print session.run(model)
