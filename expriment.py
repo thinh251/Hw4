af4ddc816bf6786ac10b6893224f57a13e12c7f3
Kunal Deshmukh <kunalrdeshmukh@gmail.com>
2017-11-18 17:07:51 -0800

    changes in expriment.py

--- expriment.py
+++ expriment.py
@@ -5,19 +5,23 @@ import subprocess
 import sys
 import util
 import sklearn
+import datetime
 
 if not conv_train.validate_arguments(sys.argv):
     sys.exit("Invalid arguments")
 
 results = []
-
+start = datetime.datetime.now()
 for i in range(1,int(sys.argv[4])):
+    a = datetime.datetime.now()
     print "-------------------------------------------"
     print "-------------------------------------------"
     print " for max updates : ",i*10
     print "-------------------------------------------"
     sys.argv[4] = i*10
     results.append(conv_train.train())
+    b = start = datetime.datetime.now()
+    print "Time required for this step: "+str(b-a)
 
 
 x_value = []
@@ -30,7 +34,7 @@ for i in range(len(results)):
 print x_value
 print y_value
 print z_value
-plt.title("Q1")
+plt.title("Q1. for '"+sys.argv[5].strip()+", cost function:"+sys.argv[1])
 plt.xlabel("Max Updates")
 plt.ylabel("Cost")
 training_line, =plt.plot(x_value,y_value,color='blue',label="Training Cost",
@@ -39,5 +43,7 @@ validation_line, = plt.plot(x_value,z_value,color='green',label="Validation "
                                                               "Cost",
          linestyle='dashed')
 plt.legend(handles=[training_line,validation_line],loc=2)
+end = datetime.datetime.now()
+print "Total time required "+str(end-start)
 plt.show()
 
